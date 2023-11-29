aaae532032f86c74c573b30468ae3917a9a3a18d
edgargama <79337709+edgargama@users.noreply.github.com>
2023-11-29 09:39:20 -0300

    index.tsx

--- src/pages/recover/index.tsx
+++ src/pages/recover/index.tsx
@@ -54,7 +54,7 @@ export default function AuthenticationPage() {
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
-            Prova do Segundo Bimestre
+            Inova Week
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
